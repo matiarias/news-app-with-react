669ff564bed82568c4767cdc71a8b0a50c3b09bb
Matias Arias <matt.arias182@gmail.com>
2022-08-29 18:02:10 -0300

    navbar menu mobile finalizado

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,27 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import newsLogo from "../../assets/newsapp-logo.png";
 import { Link } from "react-router-dom";
+import { GrClose } from "react-icons/gr";
+import { GrMenu } from "react-icons/gr";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav(!nav);
+  };
   return (
-    <div className="relative bg-gradient-to-r from-[rgba(136,73,9,.7)] to-[#335343] w-full h-[90px] p-2 lg:px-4 lg:py-2 flex justify-between items-center">
+    <div className="bg-gradient-to-r from-[rgba(136,73,9,.7)] to-[#335343] w-full h-[90px] p-2 md:px-4 md:py-2 flex justify-between items-center">
+      {/* ------------------------------------- Menu -------------------------------------- */}
+
       <Link to="/">
         <img className="h-[50px] lg:h-[70px]" src={newsLogo} alt="news logo" />
       </Link>
-      <div className="mr-8">
-        <ul className="flex gap-x-4 lg:gap-x-10">
-          <Link to="/">
-            <li className="text-white text-sm md:text-base lg:text-xl font-medium hover:text-yellow-400 lg:hover:scale-110">
-              Inicio
-            </li>
-          </Link>
-          <Link to="/about">
-            <li className="text-white text-sm md:text-base lg:text-xl font-medium hover:text-yellow-400 lg:hover:scale-110">
-              Sobre Nosotros
-            </li>
-          </Link>
-        </ul>
+
+      <ul className="hidden md:flex md:gap-x-8 md:mr-4">
+        <Link to="/">
+          <li className="text-white text-sm md:text-base lg:text-xl font-medium hover:text-yellow-400 lg:hover:scale-110">
+            Inicio
+          </li>
+        </Link>
+        <Link to="/about">
+          <li className="text-white text-sm md:text-base lg:text-xl font-medium hover:text-yellow-400 lg:hover:scale-110">
+            Sobre Nosotros
+          </li>
+        </Link>
+      </ul>
+
+      {/* ---------------------------------------- Hamburger ------------------------------------- */}
+
+      <div onClick={handleClick} className="md:hidden z-20">
+        {!nav ? (
+          <GrMenu className="text-3xl animate-pulse" />
+        ) : (
+          <GrClose className="text-3xl animate-pulse" />
+        )}
       </div>
+
+      {/* ---------------------------------- mobile menu ----------------------------------- */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-[50%] bg-gradient-to-r from-[#884909] to-[#335343] flex flex-col justify-center items-center gap-8"
+        }
+      >
+        <Link to="/">
+          <li className="text-white text-2xl font-medium hover:text-yellow-400 lg:hover:scale-110">
+            Inicio
+          </li>
+        </Link>
+        <Link to="/about">
+          <li className="text-white text-2xl font-medium hover:text-yellow-400 lg:hover:scale-110">
+            Sobre Nosotros
+          </li>
+        </Link>
+      </ul>
     </div>
   );
 };
