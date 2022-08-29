4ec7c1200f6f0c864c342ac66455150fa9b3a4c6
Matias Arias <matt.arias182@gmail.com>
2022-08-29 01:55:53 -0300

    se creo el componente noticias para que contenga el codigo de las cards

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../components/footer/Footer";
 import NavBar from "../components/navbar/NavBar";
 
 const AboutUs = () => {
   return (
-    <div>
+    <div className="w-full h-full">
       <NavBar />
+      <Footer />
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import NavBar from "../components/navbar/NavBar";
 import Overlay from "../components/overlay/Overlay";
 import Footer from "../components/footer/Footer";
 import Loading from "../components/loading/Loading";
+import NoticiasCard from "../components/noticias card/NoticiasCard";
 
 const Home = () => {
   const [noticias, setNoticias] = useState({
@@ -52,32 +53,7 @@ const Home = () => {
             </div>
           ) : (
             noticias.datos.map((item, index) => (
-              <div
-                key={index}
-                className="relative bg-[#fbfff9] h-[500px] sm:h-[550px] md:h-[600px] lg:h-[500px] w-full md:max-w-[450px] rounded-lg border-2 border-[#618f61] shadow-xl shadow-[#82a882] place-self-center"
-              >
-                <img
-                  className="w-full h-[200px] object-cover"
-                  src={item.image}
-                  alt={item.title}
-                />
-                <div className="font-medium text-center bg-gradient-to-r from-green-800 to-green-300">
-                  <span className="text-white">{item.source.name}</span>
-                </div>
-                <h3 className="text-black font-bold text-lg md:text-md lg:text-xl p-2">
-                  {item.title}
-                </h3>
-                <p className="text-black font-light px-2 py-1 text-sm md:text-md">
-                  {item.description}
-                </p>
-                <a
-                  className="absolute bottom-6 left-1/4 w-[50%] py-1 rounded-lg shadow-lg shadow-green-900 text-center text-white text-md lg:text-lg bg-gradient-to-r from-green-900 to-lime-600 hover:from-amber-800 hover:to-sky-900"
-                  href={item.source.url}
-                  target="_blank"
-                >
-                  Ir a la Noticia
-                </a>
-              </div>
+              <NoticiasCard item={item} index={index} />
             ))
           )}
         </section>
